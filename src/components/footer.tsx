c3d5ecabfc417f67d8a9f91decf3c95e3cec417d
joel <joel@reboundstudios.com>
2022-12-18 17:25:06 -0500

    v 0.9.8 r3

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,8 +5,8 @@ import { FC } from 'react';
 export const Footer: FC = () => {
   return (
     <footer className="min-h-full flex gap-8 items-center justify-center bg-black
-      text-white p-10">
-      <Image src="/logo.jpg" width={50} height={50} alt="logo" />
+      text-white p-4">
+      <Image src="/logo.jpg" width={25} height={25} alt="logo" />
       <Link href="/">Terms of Use</Link>
       <Link href="/">Privacy Policy</Link>
     </footer>

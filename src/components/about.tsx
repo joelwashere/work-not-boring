53e894e471e5c6e78ffe78fc4760f7b0d6827f6b
joel <joel@reboundstudios.com>
2023-04-23 11:36:26 -0400

    Quick update
    Updated text color and fixed refresh error in about section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,18 +3,18 @@ import { FC } from "react";
 export const About: FC = () => {
 
   return (
-    <section className="flex flex-col bg-[#f2f2f2] pt-10 pb-20 px-8 text-xl lg:2xl">
+    <section className="flex flex-col text-black bg-[#f2f2f2] pt-10 pb-20 px-8 text-xl lg:2xl">
       <p className="text-center leading-tight max-w-5xl mx-auto tracking-tight text-3xl">
         <strong>We provide solutions to help businesses worldwide solve important problems </strong>
         <br />
         <br />
-        <p className="text-xl">
+        <span className="block text-xl">
           Web 3 technologies are revolutionizing businesses by cutting out middle man,
           providing instant payment settlement, and smart contracts.
           <br />
           <br />
           <strong>Changing the way we do business forever.</strong>
-        </p>
+        </span>
       </p>
     </section>
   );
